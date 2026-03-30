1503054e6c0b99d3c40ee8213ed7f2c8ae856fa5
8Fouad8 <120306611+8Fouad8@users.noreply.github.com>
2026-03-30 15:35:33 +0200

    created Image List Item

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/assets_data.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
 
@@ -6,6 +7,31 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [CustomAppBar()]);
+    return Column(children: [CustomAppBar(), ImageListItem()]);
+  }
+}
+
+class ImageListItem extends StatelessWidget {
+  const ImageListItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .27,
+      child: AspectRatio(
+        aspectRatio: 2.5/4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.pink,
+            image: DecorationImage(
+            
+              image: AssetImage(AssetsData.testImage),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:bookly/constants.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => const MyApp())
+    );
 }
 
 class MyApp extends StatelessWidget {
