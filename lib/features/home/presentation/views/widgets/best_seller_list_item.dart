b4554e523c2653add0eb52e58a16ec8181fa8657
8Fouad8 <120306611+8Fouad8@users.noreply.github.com>
2026-03-31 15:23:59 +0200

    finished best seller Item

--- lib/features/home/presentation/views/widgets/best_seller_list_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_item.dart
@@ -1,8 +1,8 @@
-
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets_data.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BestSellerListItem extends StatelessWidget {
   const BestSellerListItem({super.key});
@@ -14,47 +14,75 @@ class BestSellerListItem extends StatelessWidget {
       child: Row(
         children: [
           AspectRatio(
-            aspectRatio: 2.5/4,
+            aspectRatio: 2.5 / 4,
             child: Container(
               height: 120,
               decoration: BoxDecoration(
                 color: Colors.pink,
                 borderRadius: BorderRadius.circular(8),
                 image: DecorationImage(
-                  image: AssetImage(AssetsData.testImage ),
+                  image: AssetImage(AssetsData.testImage),
                   fit: BoxFit.cover,
                 ),
               ),
             ),
           ),
           SizedBox(width: 20),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .6,
-                child:const Text(
-                  "Harry Potter and the Goblet of Fire",
-                  style: Styles.textStyle20,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .6,
+                  child: const Text(
+                    "Harry Potter and the Goblet of Fire",
+                    style: Styles.textStyle20,
+                  ),
                 ),
-              ),
-              SizedBox(height: 3),
-              Text(
-                "J.K. Rowling",
-                style: Styles.textStyle14.copyWith(
-                  color: Colors.grey,
-                  fontFamily: kGtSectraFine,
+                SizedBox(height: 3),
+                Text(
+                  "J.K. Rowling",
+                  style: Styles.textStyle14.copyWith(
+                    color: Colors.grey,
+                    fontFamily: kGtSectraFine,
+                  ),
                 ),
-              ),
-              SizedBox(height: 3),
-              Text(
-                "19.99 \$",
-                style: Styles.textStyle16.copyWith(fontWeight: FontWeight.w800),
-              ),
-            ],
+                SizedBox(height: 3),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "19.99 \$",
+                      style: Styles.textStyle16.copyWith(
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                    BookRating(),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class BookRating extends StatelessWidget {
+  const BookRating({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const FaIcon(FontAwesomeIcons.solidStar, color: Colors.amber, size: 16),
+        SizedBox(width: 5),
+        Text("4.8", style: Styles.textStyle16.copyWith()),
+        SizedBox(width: 5),
+
+        Text("(2123)", style: Styles.textStyle16.copyWith(color: Colors.grey)),
+      ],
     );
   }
 }
